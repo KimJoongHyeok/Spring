982cb4e9f171ead437e079a65aef9d3f5118481e
KimJoongHyeok <moon960427@gmail.com>
2021-11-27 02:44:40 +0900

    MemberVO Jpa 방식 추가

--- spring-basic/src/main/java/spring/springbasic/domain/Member.java
+++ spring-basic/src/main/java/spring/springbasic/domain/Member.java
@@ -1,9 +1,16 @@
 package spring.springbasic.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //DB에 ID값을 알아서 넣어줌
     private Long id;
     private String name;
+    //@Column(name = "username") //db의 컬럼값이 username 이라면
+
+
     public Long getId() {
         return id;
     }
